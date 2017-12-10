cccf149760eb6c3a1eb6eae14b48355e43a1721c
surfen113 <ritter.julian@googlemail.com>
2017-12-10 23:10:52 +0100

    Initial Files for Assignment 3

--- Assignment3/js/main.js
+++ Assignment3/js/main.js
@@ -1,6 +1,5 @@
-function load_charts() {
-    load_chart1();
-    load_chart2();
+function loadTree() {
+    load_chart();
 };
 
 var margin = {top: 20, right: 20, bottom: 70, left: 70},
@@ -8,7 +7,7 @@ var margin = {top: 20, right: 20, bottom: 70, left: 70},
     height = 400 - margin.top - margin.bottom;
 
 
-function load_chart1() {
+function load_chart() {
 
 
     var parseTime = d3.timeParse("%Y");
@@ -100,90 +99,3 @@ function load_chart1() {
 
 }
 
-function load_chart2() {
-
-
-    // set the ranges
-    var x = d3.scaleTime().range([0, width]);
-    var y = d3.scaleLinear().range([height, 0]);
-    
-    var parseTime = d3.timeParse("%Y");
-
-
-    // define the line
-    var valueline = d3.line()
-        .x(function (d) {
-            return x(d.yr);
-        })
-        .y(function (d) {
-            if (d.mean < 900) return y(d.mean); else return 0;
-        });
-
-    // append the svg obgect to the body of the page
-    // appends a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
-    var svg = d3.select("#chart2").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.csv("data.csv", function (error, data) {
-        if (error) throw error;
-
-        // format the data
-        data.forEach(function (d) {
-            d.yr = parseTime(d.YEAR);
-            d.mean = +d.metANN;
-        });
-
-        //@TODO: Is this even getting used?
-        avg = d3.mean(data, function (d) {
-            if (d.mean < 900) return d.mean; else return 0;
-        });
-
-        x.domain(d3.extent(data, function (d) {
-            return d.yr;
-        }));
-        y.domain(d3.extent(data, function (d) {
-            if (d.mean < 900) return d.mean; else return 0;
-        }));
-
-        // Add the valueline path.
-        svg.append("path")
-            .data([data])
-            .attr("class", "line")
-            .attr("d", valueline);
-
-        // Add the X Axis
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .attr("class", "x")
-            .call(d3.axisBottom(x));
-
-        // text label for the x axis
-        svg.append("text")
-            .attr("transform",
-                "translate(" + (width / 2) + " ," +
-                (height + margin.top + 20) + ")")
-            .attr("class", "axistext")
-            .text("Year");
-
-        // Add the Y Axis
-        svg.append("g")
-            .attr("class", "y")
-            .call(d3.axisLeft(y));
-
-        // text label for the y axis
-        svg.append("text")
-            .attr("class", "y")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left)
-            .attr("x", 0 - (height / 2))
-            .attr("dy", "1em")
-            .attr("class", "axistext")
-            .text("Mean Annual Temperature (in °C)");
-
-    });
-}
