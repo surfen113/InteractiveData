5bb7ec125e80b8bbe33be185bb684a238bbd0096
surfen113 <ritter.julian@googlemail.com>
2017-12-18 14:17:27 +0100

    Added Tooltips to 1st Graph

--- Assignment4/js/main.js
+++ Assignment4/js/main.js
@@ -15,9 +15,6 @@ function plotPCA() {
     var y = d3.scaleLinear().range([height, 0]);
 
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
     var svg = d3.select("#graph1").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -25,7 +22,11 @@ function plotPCA() {
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-// Get the data
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+
     d3.csv("hands_pca.csv", function (error, data) {
 
         if (error) throw error;
@@ -43,6 +44,7 @@ function plotPCA() {
         y.domain(d3.extent(data, function (d) {
             return d.y;
         }));
+
         // Add the valueline path.
         // svg.append("path")
         //     .data([data])
@@ -50,6 +52,7 @@ function plotPCA() {
         //     .attr("d", valueline);
 
         // Add the scatterplot
+
         svg.selectAll("dot")
             .data(data)
             .enter().append("circle")
@@ -59,6 +62,21 @@ function plotPCA() {
             })
             .attr("cy", function (d) {
                 return y(d.y);
+            })
+                
+            //Add tooltip
+            .on("mouseover", function (d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.html((d.x) + "x <br/>" + d.y + "y")
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
             });
 
         // Add the X Axis
