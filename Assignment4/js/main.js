fc973cbc8ffbf6fb5e0b24c93147086c3f676f41
surfen113 <ritter.julian@googlemail.com>
2017-12-18 13:50:06 +0100

    1st Draft for PCA Chart

--- Assignment4/js/main.js
+++ Assignment4/js/main.js
@@ -1,8 +1,75 @@
 function loadJS() {
-    load_chart1();
+    plotPCA();
 };
 
-function load_chart1() {
 
+function plotPCA() {
+
+// set the dimensions and margins of the graph
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 750 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+// set the ranges
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+    var svg = d3.select("#graph1").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+    d3.csv("hands_pca.csv", function (error, data) {
+
+        if (error) throw error;
+
+        // format the data
+        data.forEach(function (d) {
+            d.x = +d["A"];
+            d.y = +d["B"];
+        });
+
+        // Scale the range of the data
+        x.domain(d3.extent(data, function (d) {
+            return d.x;
+        }));
+        y.domain(d3.extent(data, function (d) {
+            return d.y;
+        }));
+        // Add the valueline path.
+        // svg.append("path")
+        //     .data([data])
+        //     .attr("class", "line")
+        //     .attr("d", valueline);
+
+        // Add the scatterplot
+        svg.selectAll("dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("r", 5)
+            .attr("cx", function (d) {
+                return x(d.x);
+            })
+            .attr("cy", function (d) {
+                return y(d.y);
+            });
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // Add the Y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+    });
 }
 
