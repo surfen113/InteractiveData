6c5507d06b6115c1d5cb3bcfa2c44dcb4e152b23
Simona Iuliana Toader <iulia.s.toader@gmail.com>
2018-01-03 16:40:43 +0200

    Added heatmap

--- Assignment5/js/main.js
+++ Assignment5/js/main.js
@@ -73,3 +73,77 @@ function init() {
         });
     });
 }
+
+
+      var map, heatmap;
+      var asd = [];
+
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: {lat: 37.775, lng: -122.434},
+          mapTypeId: 'roadmap'
+        });
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+          data: getPoints(),
+          map: map
+        });
+      }
+
+      function toggleHeatmap() {
+        heatmap.setMap(heatmap.getMap() ? null : map);
+      }
+
+      function changeGradient() {
+        var gradient = [
+          'rgba(0, 255, 255, 0)',
+          'rgba(0, 255, 255, 1)',
+          'rgba(0, 191, 255, 1)',
+          'rgba(0, 127, 255, 1)',
+          'rgba(0, 63, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 223, 1)',
+          'rgba(0, 0, 191, 1)',
+          'rgba(0, 0, 159, 1)',
+          'rgba(0, 0, 127, 1)',
+          'rgba(63, 0, 91, 1)',
+          'rgba(127, 0, 63, 1)',
+          'rgba(191, 0, 31, 1)',
+          'rgba(255, 0, 0, 1)'
+        ]
+        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+      }
+
+      function changeRadius() {
+        heatmap.set('radius', heatmap.get('radius') ? null : 5);
+      }
+
+      function changeOpacity() {
+        heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+      }
+
+
+      // Heatmap data: 500 Points
+      function getPoints() {
+
+        d3.json("json/sf_crime.geojson", function (json) {
+
+                 for (var i = 0; i < json.features.length; i++) {
+                    coords = json.features[i].geometry.coordinates;
+                    x = coords[1];
+
+                    y = coords[0];
+
+                    latLng1 = new google.maps.LatLng(parseFloat(x),parseFloat(y));
+latLng = new google.maps.LatLng(37.731664792649603, -122.443177432448991);
+                    asd.push(latLng1);
+
+                  }
+
+        });
+
+
+return asd;
+
+      }
