f881f5d65cad7dbcc8fd94f677c892d99dcdcec7
Riddhi <nmd123@alumni.ku.dk>
2018-01-03 21:14:41 +0100

    small changes

--- Assignment5/js/charts.js
+++ Assignment5/js/charts.js
@@ -113,7 +113,7 @@ function sortByTimeAscending(a, b) {
 function linegraph() {
 
     // set the dimensions and margins of the graph
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    var margin = {top: 40, right: 20, bottom: 30, left: 50},
         width = 1500 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
