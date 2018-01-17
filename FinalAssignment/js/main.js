e81f1f8a8a83f0cf74f07ac4dc98c744e764fbc5
surfen113 <ritter.julian@googlemail.com>
2018-01-17 20:44:22 +0100

    Sticky Footer

--- FinalAssignment/js/main.js
+++ FinalAssignment/js/main.js
@@ -1,24 +1,45 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
     $('#fullpage').fullpage({
         sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage'],
         menu: '#menu',
-        scrollingSpeed: 1000
+        scrollingSpeed: 1000,
+        afterLoad: function (anchorLink, index) {
+            var loadedSection = $(this);
+
+            //using index
+            if (index == 2) {
+                document.getElementById('footer').style.display = 'inline';
+            }
+
+            if (index == 1) {
+                document.getElementById('footer').style.display = 'none';
+            }
+
+            if (index == 3) {
+                document.getElementById('footer').style.display = 'inline';
+            }
+
+            if (index == 4) {
+                document.getElementById('footer').style.display = 'inline';
+            }
+        }
     });
+
+    //document.getElementById( 'footer' ).style.display = 'none';
 
 });
 
 
 //https://refreshless.com/nouislider/
-$(document).ready(function() {
+$(document).ready(function () {
     var fancySlider = document.getElementById('slider');
 
     noUiSlider.create(fancySlider, {
-        start: [ 1993, 2003 ],
+        start: [1993, 2003],
         connect: true,
         behaviour: 'drag-tap',
-        tooltips: [ wNumb({ decimals: 0 }), wNumb({ decimals: 0 })],
+        tooltips: [wNumb({decimals: 0}), wNumb({decimals: 0})],
         padding: 1,
         //step: 1,
         range: {
@@ -27,7 +48,7 @@ $(document).ready(function() {
         }
     });
 
-    fancySlider.noUiSlider.on('update', function(){
+    fancySlider.noUiSlider.on('update', function () {
         document.getElementsByClassName("noUi-tooltip").innerHTML = "";
         var minmax = fancySlider.noUiSlider.get();
         filterData(parseInt(minmax[0]), parseInt(minmax[1]));
@@ -48,8 +69,7 @@ $("input[name='mode']").change(function (radioChanged) {
 });
 
 
-
-$(document).on('click', '#crsDescription', function(){
+$(document).on('click', '#crsDescription', function () {
     drawDonutChart("CRS");
     $.fn.fullpage.moveTo('secondPage', 1);
 });
