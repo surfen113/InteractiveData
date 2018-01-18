658629254b85a8985e7ad7081994e8176ac2d561
surfen113 <ritter.julian@googlemail.com>
2018-01-18 16:35:30 +0100

    Design Changes

--- FinalAssignment/js/main.js
+++ FinalAssignment/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        sectionsColor: ['#1bbc9b', '#4BBFC3', 'whitesmoke', 'whitesmoke', '#ccddff'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage'],
         menu: '#menu',
         scrollingSpeed: 1000,
